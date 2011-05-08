e11cc6cc67c10fa69ff47d201d5890d2ae7f6dfd
Kenji Saito <kenji0717@gmail.com>
2011-05-08 23:12:01 +0900

    Exactly test of 3D.

--- src/com/github/kenji0717/a3cs/Test.java
+++ src/com/github/kenji0717/a3cs/Test.java
@@ -1,7 +1,11 @@
 package com.github.kenji0717.a3cs;
 
+import jp.sourceforge.acerola3d.a3.*;
+
 public class Test {
-    public static void main(String args[]) {
-        System.out.println("Test!!!");
+    public static void main(String args[]) throws Exception {
+    	A3Window w = new A3Window(300,300);
+    	Action3D a3 = new Action3D("file:///Users/ksaito/media/A3/axis/axis.a3");
+    	w.add(a3);
     }
 }
