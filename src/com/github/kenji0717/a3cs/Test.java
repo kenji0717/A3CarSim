6a15b3ad9faead125ee10a7d5db17db3574a396d
Kenji Saito <kenji0717@gmail.com>
2011-05-10 00:29:29 +0900

    add resources.

--- src/com/github/kenji0717/a3cs/Test.java
+++ src/com/github/kenji0717/a3cs/Test.java
@@ -5,7 +5,7 @@ import jp.sourceforge.acerola3d.a3.*;
 public class Test {
     public static void main(String args[]) throws Exception {
     	A3Window w = new A3Window(300,300);
-    	Action3D a3 = new Action3D("file:///Users/ksaito/media/A3/axis/axis.a3");
+    	Action3D a3 = new Action3D("x-res:///res/stk_tux.a3");
     	w.add(a3);
     }
 }
