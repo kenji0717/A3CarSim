3bac47ca691aad53902873c100aec293970537a9
Kenji Saito <kenji0717@gmail.com>
2011-06-28 10:42:39 +0900

    JavaWebStart attention

--- jws/index.mm
+++ jws/index.mm
@@ -29,6 +29,40 @@ Java Web Startによる配信
 </div>
 -->
 
+64ビット版Windowsでの注意
+----------------------------------------
+
+64ビットWindowsを使用している場合，Java Web Start
+を用いたインストールを行う場合には64ビット版
+Internet Explorerを使用して下さい．(デフォルトの
+Internet Explorerは32ビット版だと思います．)
+64ビット版Internet Explorerは「スタート」ボタン
+→「すべてのプログラム」の中に入っています．
+
+MacOSXでの注意
+----------------------------------------
+
+MacOSXにはデフォルトでJava3DのVersion1.3.1が
+インストールされています．非常に古いバージョンで
+このサイトぷのプログラムの起動に悪影響があります．
+以下の方法で無効化して下さい．
+
+* /System/Library/Java/Extensionsを開く
+* その場所にOldJava3Dという名前のフォルダを作成する．
+* /System/Library/Java/Extensionsに入っている
+  以下のファイルを新しく作成したOldJava3Dフォルダに
+  移動しておく．
+    + j3daudio.jar
+    + j3dcore.jar
+    + j3dutils.jar
+    + vecmath.jar
+    + libJ3D.jnilib
+    + libJ3DAudio.jnilib
+    + libJ3DUtils.jnilib
+
+古いJava3Dが必要になったら，移動したファイルを
+元の場所に戻せばもとどうりになります．
+
 Footer @(id:1,footer)
 ========================================
 
