754573fa2d5faf8b88f35198597497c1c5213491
Kenji Saito <kenji0717@gmail.com>
2011-08-11 17:48:56 +0900

    add a news item

--- doc/CarRace/index.mm
+++ doc/CarRace/index.mm
@@ -22,7 +22,17 @@ CarRaceに関するドキュメント．
 
 現在準備中です．
 
+### NEWS，連絡事項
+
+* 2011,08/11：車の性能に関して変更があります．
+  詳しくは以下のページを見て下さい．
+    + [車の性能の変更](./item01.html)
+
+### レーストラック情報
+
 ![stk-racetrack-skeleton.png](./stk-racetrack-skeleton.png)
+
+### サンプルプログラム
 
 * [SampleProgram.html](./SampleProgram.html)
 
