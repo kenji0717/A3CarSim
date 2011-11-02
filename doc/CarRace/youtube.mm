aefb851ee1372211d2a28781e5fcc25fe0fb6130
Kenji Saito <kenji0717@gmail.com>
2011-11-02 13:28:16 +0900

    fix.

---
+++ doc/CarRace/youtube.mm
@@ -0,0 +1,42 @@
+title: CarRaceシミュレータビデオ解説
+css: ../../main.css
+
+Header @(id:1,header)
+========================================
+
+CarRaceシミュレータビデオ解説
+----------------------------------------
+
+CarRaceシミュレータのビデオ解説です．
+
+Menu @(id:1,menu)
+========================================
+
+@include('../../menu.mm')
+
+Main @(id:1,main)
+========================================
+
+説明会2の時に録画したビデオのうち，カーレース
+部門に関する部分だけYouTubeにアップしました．
+参考にして下さい．
+
+CarRaceシミュレータについて
+----------------------------------------
+
+<iframe width="560" height="315" src="http://www.youtube.com/embed/videoseries?list=PL57A70D0A70F7DC65&amp;hl=ja_JP" frameborder="0" allowfullscreen="true">
+</iframe>
+
+
+CarRaceプログラミングについて
+----------------------------------------
+
+<iframe width="560" height="315" src="http://www.youtube.com/embed/videoseries?list=PL0A808B289CA4F8AE&amp;hl=ja_JP" frameborder="0" allowfullscreen="true">
+</iframe>
+
+
+Footer @(id:1,footer)
+========================================
+
+footer
+
