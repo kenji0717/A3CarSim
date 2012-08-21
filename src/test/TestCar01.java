947423dc5f0f41634484f94dbc256e47212c255b
Kenji Saito <kenji0717@gmail.com>
2012-08-21 12:20:35 +0900

    fix.

--- src/test/TestCar01.java
+++ src/test/TestCar01.java
@@ -5,17 +5,17 @@ import javax.vecmath.Vector3d;
 
 public class TestCar01 extends RaceCarBase {
     public void exec() {
-        Vector3d loc = getLoc();
+        Vector3d loc = getLoc();//現在の車の座標
         
-        Vector3d point = getPoint(13.0);//前方の座標
-        Vector3d dir = getDirection(13.0);//前方の方向
-        point.sub(loc);
-        point.normalize();
-        Vector3d left = getUnitVecX();
-        Vector3d front = getUnitVecZ();
+        Vector3d point = getPoint(13.0);//前方13メートルの座標
+        Vector3d dir = getDirection(13.0);//前方13メートルの方向
+        point.sub(loc);//車から見たポイントの方向
+        point.normalize();//長さを1に正規化
+        Vector3d left = getUnitVecX();//車の左向き方向
+        Vector3d front = getUnitVecZ();//車の前方方向
 
-        boolean noborizaka = point.y>0.1;
-        boolean kyuukaabu = front.dot(dir)<0.9;
+        boolean noborizaka = point.y>0.1;//10%以上の上り坂
+        boolean kyuukaabu = front.dot(dir)<0.9;//ポイント方向と車の方向の一致度が0.9以下
 
         double engineForce = 700.0; //エンジン出力
         if (noborizaka) engineForce += 200; //上り坂では加速
@@ -25,9 +25,9 @@ public class TestCar01 extends RaceCarBase {
         double drift=0.0; //ドリフトしない。
 
         setForce(engineForce,steering,breakingForce,drift);
-
-//if (engineForce>800.0)System.out.println("加速");
-//if (breakingForce>1.0)System.out.println("ブレーキ");
+//以下デバッグ用のプリント文(必要に応じてコメントを外す)
+//if (noborizaka)System.out.println("上り坂");
+//if (kyuukaabu)System.out.println("急カーブ");
 //System.out.println("Debug: "+getPoint());
 //System.out.println("Debug: "+getDirection(10.0));
 //System.out.println("Debug: "+getVel());
