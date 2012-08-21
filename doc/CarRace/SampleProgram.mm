0610bfcb0f86e38b180a42107f9844913877068b
Kenji Saito <kenji0717@gmail.com>
2012-08-21 12:35:27 +0900

    fix.

--- doc/CarRace/SampleProgram.mm
+++ doc/CarRace/SampleProgram.mm
@@ -14,13 +14,13 @@ Sample Program of CarRace
     
             Vector3d point = getPoint(13.0);//前方13メートルの座標
             Vector3d dir = getDirection(13.0);//前方13メートルの方向
-            point.sub(loc);
-            point.normalize();
+            point.sub(loc);//車から見たポイントの方向
+            point.normalize();//長さを1に正規化
             Vector3d left = getUnitVecX();//車の左向き方向
             Vector3d front = getUnitVecZ();//車の前方方向
     
-            boolean noborizaka = point.y>0.1;
-            boolean kyuukaabu = front.dot(dir)<0.9;
+            boolean noborizaka = point.y>0.1;//10%以上の上り坂
+            boolean kyuukaabu = front.dot(dir)<0.9;//ポイント方向と車の方向の一致度が0.9以下
     
             double engineForce = 700.0; //エンジン出力
             if (noborizaka) engineForce += 200; //上り坂では加速
@@ -29,7 +29,7 @@ Sample Program of CarRace
             double breakingForce = kyuukaabu?10.0:0.0;//急カーブの時ブレーキ
             double drift=0.0; //ドリフトしない。
     
-            setForce(engineForce,steering,breakingForce,drift);
+            setForce(engineForce,steering,breakingForce,drift);//実際にパラメータを設定
     //以下デバッグ用のプリント文(必要に応じてコメントを外す)
     //if (noborizaka)System.out.println("上り坂");
     //if (kyuukaabu)System.out.println("急カーブ");
@@ -41,4 +41,16 @@ Sample Program of CarRace
         }
     }
 
-各メソッドの説明はAPIのページ参照．
+このソースファイルで「ベクトル変数.dot(ベクトル変数)」という
+プログラムが2箇所ありますが，ここでは2つのベクトルの内積を計算
+しています．内積を簡単に説明すると，2つのベクトルの方向の一致度と
+それぞれのベクトルの長さを掛け合せた値です．上のプログラム
+では内積を計算しているベクトルの長さはいずれも1.0に正規化されて
+いるので，単純に2つのベクトルの方向の一致度と考えることができます．
+方向がまったく同じなら1.0，完全に逆向きなら-1.0になります．
+
+その他，各メソッドの説明はAPIのページのRaceCarBaseクラスを参照
+して下さい．
+
+* [APIのページ](http://kenji0717.github.com/A3CarSim/doc/api/)
+
